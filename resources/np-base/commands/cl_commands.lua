6638a97dcf4522d6467dfe7deafd9c95acea2908
KF5135 <matheo.bda@gmail.com>
2021-10-16 22:22:48 +0200

    🔨

---
+++ resources/np-base/commands/cl_commands.lua
@@ -0,0 +1,24 @@
+NPX.Commands = NPX.Commands or {}
+
+RegisterNetEvent("np-commands:meCommand")
+AddEventHandler("np-commands:meCommand",
+    function(user, msg)
+        local monid = PlayerId()
+        local sonid = GetPlayerFromServerId(user)
+        print('using lol')
+          if sonid ~= -1 then
+            if
+                #(GetEntityCoords(GetPlayerPed(monid)) - GetEntityCoords(GetPlayerPed(sonid))) < 8.0 and
+                    HasEntityClearLosToEntity(GetPlayerPed(monid), GetPlayerPed(sonid), 17)
+            then
+                TriggerEvent("DoHudTextCoords", msg, GetPlayerPed(sonid))
+            end
+        end
+    end)
+
+ AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    TriggerServerEvent("np-commands:buildCommands","")
+  end)
